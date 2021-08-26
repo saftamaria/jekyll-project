73dd458a0b5aea98ecd0f52db0e8c3a80deb1c38
antoneta.safta <safta.antoneta@gmail.com>
2021-08-27 01:22:02 +0300

    dev

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -5,6 +5,7 @@ $('.toggled-nav-wrapper').on('click', function(e){
 $('.nav-toggle-open-wrapper, .nav-toggle-close-wrapper, header .nav-toggle-body-overlay').on('click', function(){
   $('html').toggleClass('nav-open');
 });
+
 //Smooth Scroll
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
